b1602a420aab00547b9a87ac28066edd9ed3d0c8
Aruna Kumari <105557919+iaruna@users.noreply.github.com>
2023-03-12 12:44:32 +0000

    Added connection to db

--- database.py
+++ database.py
@@ -1,24 +1,17 @@
-# import sqlalchemy
-# print(sqlalchemy.__version__)
 from sqlalchemy import create_engine, text
+import os 
 
-data_connection_string = "mysql+pymysql://znf6ei2yu3dvkncvwp8t:pscale_pw_pHrwTMmFufpycmApW0mIorj9w6R10CUWmspkbJZqFpq@ap-south.connect.psdb.cloud/careers?charset=utf8mb4"
-engine = create_engine(data_connection_string,
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print("type(result):", type(result))
-
-  result_all = result.all()
-  print("type(result.all()):", type(result_all))
-  # print("result.all():", result_all)
-
-  first_result = result_all[0]
-  print("type(first_result):", type(first_result))
-
-  first_result_dict = result_all[0]._mapping
-  print("type(first_result_dict):", type(first_result_dict))
-  print(first_result_dict)
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._mapping)
+    return jobs
